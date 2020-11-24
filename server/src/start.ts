4afff3aeec044030798bcc89726d39386ff45e84
Gabriel de Russo e Carmo <gabrielrc@fb.com>
2020-11-24 16:21:20 +0000

    socket.io and cookies

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cookie from 'cookie';
+
+const USER_COOKIE = 'hanabi_user';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -10,6 +15,21 @@ router.get('/', (req, res) => {
 
 app.use('/api', router);
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+server.listen(PORT, () => {
   console.log('listening on *:' + PORT);
+});
+
+const io = new Server(server, {
+  path: "/lobby/ABCD",
+  serveClient: false,
+});
+
+io.on('connection', (socket) => {
+  const cookies = cookie.parse(socket.handshake.headers.cookie);
+  // TODO: what if cookie is not present?
+  console.log(cookies[USER_COOKIE]);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
