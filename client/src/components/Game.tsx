1afac3dfbca8c0d5d24d0f67753853976466a269
Gabriel de Russo e Carmo <gabrielrc@fb.com>
2020-11-26 22:40:19 +0000

    add buttons to play and discard cards

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -2,22 +2,38 @@ import { ICard, IColor, IGame } from "hanabi-interface";
 
 interface CardProps {
   card: ICard,
+  hidden: boolean,
 }
 
 function Card(props: CardProps) {
-  const { card } = props;
-  return <div style={{color: IColor[card.color], display: "inline"}}>{card.value}■</div>;
+  const { card, hidden } = props;
+  if (hidden) {
+    return <div style={{ color: "black", display: "inline" }}>?■</div>;
+  }
+  return <div style={{ color: IColor[card.color], display: "inline" }}>{card.value}■</div>;
 }
 
 interface CardListProps {
   cards: ICard[],
+  myCards: boolean,
+  myTurn: boolean,
+  playFn: (c: ICard) => void;
+  discardFn: (c: ICard) => void;
 }
 
 function CardList(props: CardListProps) {
-  const { cards } = props;
+  const { cards, myCards, myTurn, playFn, discardFn } = props;
   return (
     <>
-      {cards.map(c => <Card card={c} />)}
+      {cards.map(c => {
+        return (
+          <>
+            <Card card={c} hidden={myCards} />
+            {myCards && <button onClick={() => playFn(c)} disabled={!myTurn}>Play</button>}
+            {myCards && <button onClick={() => discardFn(c)} disabled={!myTurn}>Discard</button>}
+          </>
+        );
+      })}
     </>
   );
 }
@@ -25,16 +41,40 @@ function CardList(props: CardListProps) {
 interface GameProps {
   game: IGame,
   playerIndex: number,
+  socket: SocketIOClient.Socket,
 }
 
 function Game(props: GameProps) {
-  const { game, playerIndex } = props;
+  const { game, playerIndex, socket } = props;
+
+  const playFn = (card: ICard) => socket.emit('play', card);
+  const discardFn = (card: ICard) => socket.emit('discard', card);
+  const hintFn = () => socket.emit('hint');
+
+  const myTurn = game.currentPlaying === playerIndex;
+
   return (
     <div>
       <h1>In game</h1>
+      <h2>Hints: {game.hints}</h2>
+      <button onClick={hintFn} disabled={!myTurn}>Give hint</button>
+      <h2>Lives: {game.lives}</h2>
       <ul>
-  {game.playersInOrder.map(p => <li>{p.index}: {p.index === playerIndex && <CardList cards={p.cardsInOrder} />}</li>)}
+        {game.playersInOrder.map(p => {
+          const myCards = p.index === playerIndex;
+          return <li>
+            {p.index}:
+            <CardList
+              cards={p.cardsInOrder}
+              myTurn={myTurn}
+              myCards={myCards}
+              playFn={playFn}
+              discardFn={discardFn}
+            />
+          </li>;
+        })}
       </ul>
+      {myTurn && <h2>It's your turn :)))))</h2>}
     </div>
   );
 }
