1e21dc626c559eda202e52319a6c5e0c917f5b09
Gabriel de Russo e Carmo <gabrielrc@fb.com>
2020-12-13 15:28:28 -0300

    render everything on a grid

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,4 +1,5 @@
-import { ICard, IColor, IGame } from "hanabi-interface";
+import { ICard, IColor, IGame, IPlayer } from "hanabi-interface";
+import { Container, Row, Col } from 'react-grid-system';
 
 interface CardProps {
   card: ICard,
@@ -39,11 +40,7 @@ function CardList(props: CardListProps) {
   );
 }
 
-interface DiscardProps {
-  cardsWithCount: [ICard, number][]
-}
-
-function Discard(props: DiscardProps) {
+function Discard(props: { cardsWithCount: [ICard, number][] }) {
   const { cardsWithCount } = props;
   let cards: ICard[] = [];
   cardsWithCount.forEach(([card, count]) => {
@@ -52,23 +49,101 @@ function Discard(props: DiscardProps) {
     }
   });
   // TODO: Use a proper component here instead of CardList
-  return <CardList cards={cards} myTurn={false} myCards={false} playFn={() => {}} discardFn={() => {}} canDiscard={false}/>;
+  return <CardList cards={cards} myTurn={false} myCards={false} playFn={() => { }} discardFn={() => { }} canDiscard={false} />;
 }
 
-interface FireworkProps {
-  colorsWithCount: [IColor, number][]
-}
-
-function Fireworks(props: FireworkProps) {
+function Fireworks(props: { colorsWithCount: [IColor, number][] }) {
   const { colorsWithCount } = props;
   // TODO: return a proper component here
   return (
     <>
-      {colorsWithCount.map(([color, count]) => 
+      {colorsWithCount.map(([color, count]) =>
         <div style={{ color: IColor[color], display: "inline" }}>{count}■</div>)
       }
     </>
   );
+}
+
+function Deck(props: { cardsRemaining: number }) {
+  const { cardsRemaining } = props;
+  return <h1>Cards: {cardsRemaining}</h1>
+}
+
+function Tokens(props: { lives: number, hints: number }) {
+  const { lives, hints } = props;
+  return (
+    <>
+      <h1>Hints: {hints}</h1>
+      <h1>Lives: {lives}</h1>
+    </>
+  );
+}
+
+function OtherPlayer(props: { player: IPlayer }) {
+  const { player } = props;
+  // TODO: Make CardList more generic so we don't have to supply
+  // things like playFn to other players
+  return <>
+    <h1>Player {player.index}</h1>
+    <CardList
+      cards={player.cardsInOrder}
+      myTurn={false}
+      myCards={false}
+      playFn={(c: ICard) => {}}
+      discardFn={(c: ICard) => {}}
+      canDiscard={false} />
+  </>;
+}
+
+interface MainPlayerProps {
+  player: IPlayer,
+  playFn: (c: ICard) => void,
+  discardFn: (c: ICard) => void,
+  hintFn: () => void,
+  canDiscard: boolean,
+  currentPlaying: boolean,
+}
+
+function MainPlayer(props: MainPlayerProps) {
+  const { player, playFn, discardFn, hintFn, canDiscard, currentPlaying } = props;
+  return <>
+    <h1>Player {player.index}</h1>
+    <CardList
+      cards={player.cardsInOrder}
+      myTurn={currentPlaying}
+      myCards={true}
+      playFn={playFn}
+      discardFn={discardFn}
+      canDiscard={canDiscard} />
+  </>;
+}
+
+// Compute where to display the players
+// If we numbers the columns as
+// 1 | 2 | 3
+// 4 | 5 | 6
+// 7 | 8 | 9
+// We map each index (1-based) to each player.
+function computeOtherPlayersMap(playersInOrder: IPlayer[], mainPlayerIndex: number): Map<number, IPlayer> {
+  const N = playersInOrder.length;
+  let map = new Map<number, IPlayer>();
+  if (N == 2) {
+    map.set(2, playersInOrder[(mainPlayerIndex + 1) % N]);
+  } else if (N == 3) {
+    map.set(4, playersInOrder[(mainPlayerIndex + 1) % N]);
+    map.set(6, playersInOrder[(mainPlayerIndex + 2) % N]);
+  } else if (N == 4) {
+    map.set(4, playersInOrder[(mainPlayerIndex + 1) % N]);
+    map.set(2, playersInOrder[(mainPlayerIndex + 2) % N]);
+    map.set(6, playersInOrder[(mainPlayerIndex + 3) % N]);
+  } else if (N == 5) {
+    map.set(4, playersInOrder[(mainPlayerIndex + 1) % N]);
+    map.set(2, playersInOrder[(mainPlayerIndex + 2) % N]);
+    map.set(3, playersInOrder[(mainPlayerIndex + 3) % N]);
+    map.set(6, playersInOrder[(mainPlayerIndex + 4) % N]);
+  }
+  // TODO: handle the case where N is not between 2 and 5
+  return map;
 }
 
 interface GameProps {
@@ -84,34 +159,49 @@ function Game(props: GameProps) {
   const discardFn = (card: ICard) => socket.emit('discard', card);
   const hintFn = () => socket.emit('hint');
 
-  const myTurn = game.currentPlaying === playerIndex;
+  const otherPlayersMap = computeOtherPlayersMap(game.playersInOrder, playerIndex);
+  const playerPos2 = otherPlayersMap.get(2);
+  const playerPos3 = otherPlayersMap.get(3);
+  const playerPos4 = otherPlayersMap.get(4);
+  const playerPos6 = otherPlayersMap.get(6);
+
+  const mainPlayer =
+    <MainPlayer
+      player={game.playersInOrder[playerIndex]}
+      playFn={playFn}
+      discardFn={discardFn}
+      hintFn={hintFn}
+      currentPlaying={game.currentPlaying === playerIndex}
+      canDiscard={game.hints < game.maxHints}
+    />;
 
   return (
-    <div>
-      <h1>In game</h1>
-      <h2>Hints: {game.hints}</h2>
-      <button onClick={hintFn} disabled={!myTurn}>Give hint</button>
-      <h2>Lives: {game.lives}</h2>
-      <h2>Fireworks: <Fireworks colorsWithCount={game.fireworks.inner} /></h2>
-      <h2>Discard: <Discard cardsWithCount={game.discard.inner}/></h2>
-      <ul>
-        {game.playersInOrder.map(p => {
-          const myCards = p.index === playerIndex;
-          return <li>
-            {p.index}:
-            <CardList
-              cards={p.cardsInOrder}
-              myTurn={myTurn}
-              myCards={myCards}
-              playFn={playFn}
-              discardFn={discardFn}
-              canDiscard={game.hints < game.maxHints}
-            />
-          </li>;
-        })}
-      </ul>
-      {myTurn && <h2>It's your turn :)))))</h2>}
-    </div>
+    // The game is drawn on 3x3 grid
+    //
+    //  deck     |    player   | player 
+    //----------------------------------
+    //  player   |  fireworks  | player
+    //----------------------------------
+    //  discard  | main player | tokens
+    //
+    // Player positions vary with the size of the game
+    <Container style={{ height: "100vh" }}>
+      <Row debug style={{ height: "25%" }}>
+        <Col debug><Deck cardsRemaining={game.remainingCards} /></Col>
+        <Col debug>{playerPos2 && <OtherPlayer player={playerPos2} />}</Col>
+        <Col debug>{playerPos3 && <OtherPlayer player={playerPos3} />}</Col>
+      </Row>
+      <Row debug style={{ height: "50%" }}>
+        <Col debug>{playerPos4 && <OtherPlayer player={playerPos4} />}</Col>
+        <Col debug><Fireworks colorsWithCount={game.fireworks.inner} /></Col>
+        <Col debug>{playerPos6 && <OtherPlayer player={playerPos6} />}</Col>
+      </Row>
+      <Row debug style={{ height: "25%" }}>
+        <Col debug><Discard cardsWithCount={game.discard.inner} /></Col>
+        <Col debug>{mainPlayer}</Col>
+        <Col debug><Tokens hints={game.hints} lives={game.lives} /></Col>
+      </Row>
+    </Container>
   );
 }
 
